0c6154afb32044db4891e785d378c4ca66476cd2
Franco Pari <francopari@Francos-MacBook-Pro.local>
2022-08-31 13:51:03 -0400

    migrated categories context to redux as well as implementing logger and middleware

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 
+import { setCategories } from "../../store/categories/categories.action";
+
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesArray = await getCategoriesAndDocuments("categories");
+      dispatch(setCategories(categoriesArray));
+    };
+
+    getCategoriesMap();
+  }, []);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
